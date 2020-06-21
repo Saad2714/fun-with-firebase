9a1b4f67ae919b21445a1a709ea9e5d5812a37b8
Saad Patel <patelsaadn@gmail.com>
2020-06-22 00:48:41 +0530

    Add SignIn screen, forms

--- firebaseAuth/screens/SignInScreen.js
+++ firebaseAuth/screens/SignInScreen.js
@@ -1,23 +1,95 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  Image,
+  TouchableOpacity
+} from "react-native";
+import * as firebase from "firebase";
+import { Form, Item, Input, Label, Button } from "native-base";
 
+export default class SigninScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
 
-import * as firebase from 'firebase';
+  static navigationOptions = {
+    title: "SignIn",
+  };
 
+  signInUser = (email, password) => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.props.navigation.replace("Home");
+      })
+      .catch(error => {
+        alert(error.message);
+      });
+  };
 
-
-// firebase.initializeApp(firebaseConfig);
-
-
-export default class SignInScreen extends React.Component{
-    render(){
-  return (
-    <View style={styles.container}>
-      <Text>Hey this is your Sign In Screen !
-      </Text>
-    </View>
-  );
-}
+  render() {
+    return (
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior="position"
+        enabled
+      >
+        <View style={styles.logoContainer}>
+          <Image source={require("../assets/anchor3.png")} />
+          <Text>Hello  Sign In screen</Text>
+        </View>
+        <Form style={styles.form}>
+          <Item floatingLabel>
+            <Label>Email</Label>
+            <Input
+              autoCorrect={false}
+              autoCapitalize="none"
+              keyboardType="email-address"
+              onChangeText={email => this.setState({ email })}
+            />
+          </Item>
+          <Item floatingLabel>
+            <Label>Password</Label>
+            <Input
+              secureTextEntry={true}
+              autoCorrect={false}
+              autoCapitalize="none"
+              keyboardType="email-address"
+              onChangeText={password => this.setState({ password })}
+            />
+          </Item>
+          <Button
+            style={styles.button}
+            full
+            rounded
+            onPress={() => {
+              this.signInUser(this.state.email, this.state.password);
+            }}
+          >
+            <Text style={styles.buttonText}>Sign in</Text>
+          </Button>
+        </Form>
+        <View style={styles.footer}>
+          <Text>OR</Text>
+          <TouchableOpacity
+            onPress={() => {
+              this.props.navigation.navigate("SignUp");
+            }}
+          >
+            <Text>Create a new Account ?</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -28,12 +100,13 @@ const styles = StyleSheet.create({
   logoContainer: {
     alignItems: "center",
     marginTop: 100,
-    marginBottom: 100
+    // marginBottom: 5
   },
   form: {
     padding: 20,
     width: "100%",
-    marginBottom: 30
+    marginBottom: 30,
+    // marginTop: 5
   },
   button: {
     marginTop: 20
